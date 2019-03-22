2239bf50e8a2df167c5c401f583e49a37d0bb8f2
agrup <agustinrodriguez206@gmail.com>
2019-03-22 01:58:45 -0300

    first commit

---
+++ tp2_analisis_texto/ejecicioClase1.py
@@ -0,0 +1,60 @@
+import sys
+import getopt
+from os import listdir
+from os.path import isdir
+
+
+
+def main(argv):
+	dirname = ""
+	empty = True
+
+	try:
+		opts, args= getopt.getopt(argv,"hd:n",['help','dir=','empty'])
+	except getopt.GetoptError:
+		print("pareserIR")
+		print(" -d path to source directory <origin>")
+		print(" -n without empty words")
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt=='-h':
+			print("pareserIR")
+			print(" -d path to source directory <origen>")
+			print(" -n without empty words")
+			exit()
+		elif opt in ("-d","--dir"):
+			dirname = arg
+		elif opt in ("-n","--empty"):
+			empty = False	
+
+	word_count = 0
+	word_dic = {}
+
+
+	if isdir(dirname):
+		files = listdir(dirname)
+		outputFile = open("concatenacion.txt",'w')
+		print(files)	
+		for fileI in files:
+			lines = open(dirname+'/'+fileI,'r').readlines()
+			for line in lines:
+				outputFile.write(line)
+				words = line.split()
+				for word in words:
+					word_count+=1
+					if(word in word_dic):
+						word_dic[word]+=1
+					else:
+						word_dic.update({word:1})
+		outputFile.write("Cantidad de palabras: "+ str(word_count)+"\n")
+		outputFile.write("Cantidad de palabras unicas: "+str(len(word_dic.keys()))+"\n")
+		outputFile.write("Mas frecuentes:"+"\n")
+		ordered_keys = sorted(word_dic, key=word_dic.get, reverse=True)
+			
+		for i in range(0,5):
+			outputFile.write(ordered_keys[i]+": "+ str(word_dic[ordered_keys[i]])+"\n") 
+	outputFile.close()	
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
