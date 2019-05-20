314ab718ef0087cebfd3a9864c1f60b45273b837
agrup <agustinrodriguez206@gmail.com>
2019-05-20 00:16:02 -0300

    parser qry with terms repeat and not repeat

--- tp4_Modelos_recuperacion/cisi_query_parser.py
+++ tp4_Modelos_recuperacion/cisi_query_parser.py
@@ -5,6 +5,7 @@ dircisi = "/home/agu/Unlu/IR/colecciones/cisi/"
 QRY="CISI.QRY"
 REL = "CISI.REL"
 TERM= "CISI.terms"
+QRY_ONCE="CISI_ONCE.QRY"
 
 def parse_query(dircisi):
     try:
@@ -37,7 +38,7 @@ def parse_query(dircisi):
                     #print(lines[i])
                     i += 1
                     newQry = []
-                    
+                    newQry_once=[]
                     tmp = {}
                     #print("new query",id)
                     #queries[id]
@@ -48,28 +49,57 @@ def parse_query(dircisi):
                         #print (newterms)
                         for term in newterms.split():
                             if term in terms:
-                                newQry.append(term)                    
+                                newQry.append(term)
+                                if term not in newQry_once:
+                                    newQry_once.append(term)             
                         i+=1
                     
                     #print(newQry)
-                    queries[id]=newQry
+                    queries[id]=(newQry , newQry_once)
+                    #queries[id]=(newQry)
+                    
                 
                 id+=1
-        with open(QRY+".txt",'a',encoding='utf-8') as qry_result:
-            for query, terms in queries.items():
+        with open(QRY+".txt",'a',encoding='utf-8') as qry_result,\
+            open(QRY_ONCE+".txt",'a',encoding='utf-8') as qry_once_result:
+            for query, terms_tup in queries.items():
                 #print("id qry",query)
                 #print("terminos",str(terms))
+                #print(terms)
                 qry_str=""
+                qry_once_str=""
+                
+                #termino repetidos
                 qry_result.write('<TOP>\n')
                 qry_result.write('<NUM>'+str(query)+'</NUM>\n')
                 qry_result.write('<TITLE>')
+
+                #terminos no repetidos
+                qry_once_result.write('<TOP>\n')
+                qry_once_result.write('<NUM>'+str(query)+'</NUM>\n')
+                qry_once_result.write('<TITLE>')
+
                 #qry_result.write(str(query))
+
+                terms , terms_once = terms_tup
+
                 for term in terms:
                     qry_str+=term+" "
+                
+                for term in terms_once:
+                    qry_once_str+=term+" "
                 #print(qry_str)
+
+                #terminos repetidos
                 qry_result.write(qry_str)
                 qry_result.write('</TITLE>\n')
                 qry_result.write('</TOP>\n')
+                
+                #terminos sin repetir
+                qry_once_result.write(qry_once_str)
+                qry_once_result.write('</TITLE>\n')
+                qry_once_result.write('</TOP>\n')
+
         #print(queries)
     except IOError as e:
         print (e)
